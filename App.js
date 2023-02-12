7f3875045e71a53e2eb6b2fa972a90b6d6808d9e
MUSICPLAYERLSPT <125202989+MUSICPLAYERLSPT@users.noreply.github.com>
2023-02-12 16:22:51 +0000

    update test

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Container 1</Text>
+      <StatusBar style="auto" />
+    </View>
+    <View style={styles.container} backgroundColor="#fae">
+      <Text>Container 2</Text>
       <StatusBar style="auto" />
     </View>
   );
